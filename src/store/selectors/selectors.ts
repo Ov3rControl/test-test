547032ab9840245c09054f427138d96786bfa842
Mohamed Adel Ibrahim <mohamed.adel@joovlly.com>
2020-11-30 12:50:06 +0200

    :sparkles: add state filtering selector

--- src/store/selectors/selectors.ts
+++ src/store/selectors/selectors.ts
@@ -1,1 +1,19 @@
-export const ww = 2;
+import { selector } from "recoil";
+import { repoListFilterState, repoListState } from "../atoms/atom";
+
+export const filteredRepoListState = selector({
+  key: "filteredRepoListState",
+  get: ({ get }) => {
+    const filter = get(repoListFilterState);
+    const list = get(repoListState);
+
+    switch (filter) {
+      case "Show All":
+        return list;
+      case "Show Starred Only":
+        return JSON.parse(localStorage.getItem("starredRepoList") || "[]");
+      default:
+        return list;
+    }
+  },
+});
